cc90e8a943f52b8ea73ccfef856a447c05848786
Jack <jebulger1003@gmail.com>
2023-04-19 12:11:27 -0400

    finished application, removed console.logs, removed ToDo comments, and populated readme

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
 # Work Day Scheduler
 
 ## About
-
+As a student with a busy schedule, it is crucial that I find ways to effectively manage my time. This application will help me do just that. It serves as a day planner, where I can schedule out activities and tasks that need to be completed. Each time slot has a save button, where the tasks can be saved into the browser's local storage. Upon reloading the page, any previously saved tasks will be generated onto the page into their according time blocks. The application can be used to plan out the day, and it can be closed out and reopened at any point for reference. This project was also my first one working with JQuery, so I now have more understanding of how it is written and used within an applicaiton.
 ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
+- [Features](#features)
 - [Deployed-Application](#deployed-application)
 
 ## Installation
-
+All that is required is a device with access to an internet browser.
 ## Usage
+The application can be used to plan out your day. The time blocks run from 9-5, a typical day for most full-time jobs. Type in your task at the time you want to be working on it and click the save button to the right if you want it to persist.
+
+After planning out your day, you can close the application, if you want, and reopen it for reference whenever you need.
+
+At any point, you can change whichever task to a different one and save to reflect that change.
+
+## Features
+The time blocks will be color coded based on past, present, and future tense. The applicaiton checks the current time and color codes the blocks based on that current time. 
+
+Grey colored time blocks are in the past, red is in the present, and any green time blocks are yet to come in the future.
+
+For example, the time was 11AM when this image was taken: 
+### ![](./assets/images/05-hw-color-feature.png)
+
+Upon loading the page, the current date will be displayed at the top of the page:
+### ![](./assets/images/05-hw-time-feature.png)
+
+The save buttons are specific to their adjacent time blocks, so the save button for the 9AM time block will only save what is typed inside that time block.
+
+Example here:
+### ![](./assets/images/05-hw-save-feature_AdobeExpress.gif)
 
 ## Deployed Application
+**Live URL Here:** [_https://jebulger.github.io/work-day-scheduler/_](_https://jebulger.github.io/work-day-scheduler/)
+
+### ![](./assets/images/05-hw-2-full-size.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // after page loads, any previously stored toDo items will be generated
+  // After page loads, any previously stored toDo items will be generated
   // onto the page.
   var currentDayEl = document.getElementById("currentDay");
-  var toDos = readLocalStorage();
+  readLocalStorage();
   storageToPage();
-  console.log(toDos);
   applyTense();
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   
   // Event listener for the save buttons
   $(".saveBtn").click(function() {
@@ -39,18 +28,11 @@ $(function () {
       }
     }
     addToStorage(toDos);
-    console.log(toDos);
   })
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
   // When the page loads, this function is called and will go and set the
   // class of the timeblocks based on if they are in the present, past, or
-  // future
+  // future tense.
   function applyTense() {
     var timeNow = dayjs().format('HH');
     var hour9 = $( '#hour-9' );
@@ -181,9 +163,6 @@ $(function () {
       hour17.removeClass('present');
     }
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?4
 
   // This function checks local storage for any previously saved toDos
   function readLocalStorage() {
@@ -271,12 +250,10 @@ $(function () {
     }
   }
 
-  // Function for finding index;
+  // Function used for 'findIndex()'
   function time(item) {
     return item;
   }
-
-  // TODO: Add code to display the current date in the header of the page.
 
   // Grabs current time and stores into variable
   // Then displays the time into the header
